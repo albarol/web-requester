6a67cfe084d4751c1cb711bb703bce2f293a7777
Alexandre Oliveira <fakeezz@gmail.com>
2012-10-31 11:48:45 -0200

    include tests to head verb

---
+++ app/tests/WebRequester.Tests/Helpers/HttpHeadProcessor.cs
@@ -0,0 +1,22 @@
+﻿namespace WebRequester.Tests.Helpers
+{
+    using System.IO;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+
+    [ServiceContract]
+    public class HttpHeadProcessor
+    {
+        [WebInvoke(Method = "HEAD", UriTemplate = "/Head")]
+        public Stream Head()
+        {
+            var woc = WebOperationContext.Current;
+            var ir = woc.IncomingRequest;
+            var or = woc.OutgoingResponse;
+
+            var uri = ir.UriTemplateMatch.RequestUri;
+            or.StatusCode = System.Net.HttpStatusCode.OK;
+            return null;
+        }
+    }
+}
---
+++ app/tests/WebRequester.Tests/HttpHeadFixture.cs
@@ -0,0 +1,59 @@
+﻿namespace WebRequester.Tests
+{
+    using System.Net;
+
+    using NUnit.Framework;
+
+    using SharpTestsEx;
+
+    using WebRequester.Tests.Helpers;
+
+    [TestFixture]
+    public class HttpHeadFixture
+    {
+        private HttpRequester requester;
+
+        private WebRequesterHost host;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.requester = new HttpRequester();
+            this.host = new WebRequesterHost(typeof(HttpHeadProcessor));
+            this.host.Open();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.host.Close();
+        }
+
+        [Test]
+        public void Head_ShouldReturn200WhenGetHead()
+        {
+            // Arrange:
+            const string Uri = "http://localhost:5555/Head";
+
+            // Act:
+            var response = this.requester.Head(Uri);
+
+            // Assert:
+            response.HttpStatusCode.Should().Be.EqualTo(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void Head_ShouldReturn200WhenGetHeadWithHeaders()
+        {
+            // Arrange:
+            const string Uri = "http://localhost:5555/Head";
+            var headers = new { token = "token" };
+
+            // Act:
+            var response = this.requester.Head(Uri, headers);
+
+            // Assert:
+            response.HttpStatusCode.Should().Be.EqualTo(HttpStatusCode.OK);
+        }
+    }
+}
